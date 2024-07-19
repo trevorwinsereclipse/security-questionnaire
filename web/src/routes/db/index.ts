e1be196367dcaa9e2f9253dc3945eeeae97786d8
trevorwinsereclipse <trevor.winser@eclipse-foundation.org>
2024-07-19 10:08:19 -0400

    Small docker compose changes
    - Changed docker-compose.yml to have proper volumes

--- web/src/routes/db/index.ts
+++ web/src/routes/db/index.ts
@@ -1,13 +1,13 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+// import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+// import { drizzle } from "drizzle-orm/node-postgres";
+// import { Pool } from "pg";
 
-const pool = new Pool({
-  host: "127.0.0.1",
-  port: 5432,
-  user: "postgres",
-  password: "password",
-  database: "db_name",
-});
+// const pool = new Pool({
+//   host: "web-database-1",
+//   port: 5432,
+//   user: "postgres",
+//   password: "password",
+//   database: "db_name",
+// });
 
-const db = drizzle(pool);
+// const db = drizzle(pool);
