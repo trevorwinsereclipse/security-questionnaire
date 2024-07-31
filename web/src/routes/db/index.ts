2379457dc64f2a2f93a379d0b27eb342d2078528
trevorwinsereclipse <trevor.winser@eclipse-foundation.org>
2024-07-31 14:42:32 -0400

    Small changes connection fix attempt
    - Attempted to fix the ECONNREFUSED error with a solution on stackoverflow by changing the host name to the container name for the database.

--- web/src/routes/db/index.ts
+++ web/src/routes/db/index.ts
@@ -1,14 +1,14 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
-import { drizzle } from "drizzle-orm/node-postgres";
+// import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
+// import { drizzle } from "drizzle-orm/node-postgres";
 import { Client } from "pg";
 
 const client = new Client({
-  host: "127.0.0.1",
+  host: "postgres",
   port: 5432,
-  user: "postgres",
-  password: "password",
-  database: "db_name",
+  user: "user123",
+  password: "securepassword",
+  database: "postgres",
 });
 
 await client.connect();
-const db = drizzle(client);
+// const db = drizzle(client);
