5903ef2f8b79077220b06d82ee029615113ab865
trevorwinsereclipse <trevor.winser@eclipse-foundation.org>
2024-08-01 08:02:48 -0400

    Connection Successful
    - Working connection to docker database. Established server with new .env credentials

--- web/src/routes/db/index.ts
+++ web/src/routes/db/index.ts
@@ -1,14 +1,28 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+return "Hello World!!!"
+});
 // import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
 // import { drizzle } from "drizzle-orm/node-postgres";
 import { Client } from "pg";
 
 const client = new Client({
-  host: "postgres",
+  host: "web-database-1",
   port: 5432,
-  user: "user123",
+  user: "postgres",
   password: "securepassword",
   database: "postgres",
 });
 
-await client.connect();
-// const db = drizzle(client);
+
+await client.connect()
+ 
+try {
+   const res = await client.query('SELECT $1::text as message', ['Hello world!'])
+   console.log(res.rows[0].message) // Hello world!
+} catch (err) {
+   console.error(err);
+} finally {
+   await client.end()
+}
